8696d9ecc91d316ceb76d79cc4b62ec37433e165
tkny <zhtony0398@gmail.com>
2023-08-09 03:40:11 -0400

    init localstream

---
+++ webrtc/src/components/VideoStream/index.tsx
@@ -0,0 +1,8 @@
+// let localStream; // This is the data stream for audio/video from your device
+// let remoteStream; // This is the data stream for audio/video from the device you are communicating with
+
+// let init = async () => {
+  // This asks the browser for permission to access video and audio
+//   localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+//   document.getElementById('id').srcObject = localStream;
+// }
