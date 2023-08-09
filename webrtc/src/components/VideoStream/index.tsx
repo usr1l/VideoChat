3c6a521b060e839278180b90f1b3602718347bbe
tkny <zhtony0398@gmail.com>
2023-08-09 03:43:51 -0400

    createOffer

--- webrtc/src/components/VideoStream/index.tsx
+++ webrtc/src/components/VideoStream/index.tsx
@@ -1,8 +1,20 @@
 // let localStream; // This is the data stream for audio/video from your device
 // let remoteStream; // This is the data stream for audio/video from the device you are communicating with
+// let peerConnection; // connect to another user
 
 // let init = async () => {
   // This asks the browser for permission to access video and audio
 //   localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
 //   document.getElementById('id').srcObject = localStream;
+// }
+
+// let createOffer = async() => {
+  // peerConnection = new RTCPeerConnection();
+  // remoteStream = new MediaStream();
+
+  // document.getElementById('id').srcObject = remoteStream;
+
+  // let offer = await peerConnection.createOffer();
+  // await peerConnection.setLocalDescription(offer);
+
 // }
